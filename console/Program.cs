067d82defd70236f48c7e2429573dbcf1e051993
Arjun Jobanputra <ajobanpu@microsoft.com>
2018-08-06 12:33:43 -0700

    initial commit

---
+++ console/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(Assembly.GetCallingAssembly().FullName);
+            Console.ReadKey();
+        }
+    }
+}
